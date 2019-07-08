f8c5fb98d857a74431e0988a990ac2cbd04dae5d
aemcee <acapili07@gmail.com>
2019-07-08 02:19:51 +0000

    completed static express server

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 // Build Static Webserver Here
+const express = require('express');
+const path = require('path');
+const server = express();
+
+// serve the folder we're in statically
+// what we built on the server branch static is doing below
+server.use(express.static(path.resolve(__dirname))); 
+
+server.listen(3001, () => {
+    console.log('Server is running at localhost:3001');
+}).on('error', (error) => {
+    console.log('Error listening on PORT:3001. Do you already have a server running on 3001');
+});
